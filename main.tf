78ce0b0726ba67f93f5be6a589f91cecd4d7442a
Zamananwar <zamananwar856@gmail.com>
2025-01-08 16:59:20 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,4 +31,4 @@ resource "aws_instance" "example" {
   instance_type = "t2.micro"  # Instance type can be adjusted based on your requirements
 
   # Use the existing SSH key pair in AWS (replace with your key name)
-  key_name      = "key-049413711"
+  key_name      = "key-049413711"}
