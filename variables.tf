fe0a197623b094d7ad22da9559919212adbde652
Zamananwar <zamananwar856@gmail.com>
2025-01-09 16:37:18 +0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+# variables.tf
+
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AKIAYM7PNZZ2MXZCYRT5"
+  type        = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "sItezL66lKroVC5uyD6K/sg/oMZ8ccyR6E5aE2Vq"
+  type        = string
+}
