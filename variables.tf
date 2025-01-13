b3c2a7ef63dd2f2761b23fb357adac82f97e2609
Zamananwar <zamananwar856@gmail.com>
2025-01-13 15:15:13 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,31 @@
 # variables.tf
 
 variable "AWS_ACCESS_KEY_ID" {
-  description = "AKIAYM7PNZZ2MXZCYRT5"
+  description = "The AWS access key ID"
   type        = string
+  sensitive   = true
 }
 
 variable "AWS_SECRET_ACCESS_KEY" {
-  description = "sItezL66lKroVC5uyD6K/sg/oMZ8ccyR6E5aE2Vq"
+  description = "The AWS secret access key"
   type        = string
+  sensitive   = true
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+  default     = "vpc-0c94ff138dc12f1ac"  # Replace with your VPC ID
+}
+
+variable "subnet_id" {
+  description = "The ID of the subnet"
+  type        = string
+  default     = "subnet-08d6da63096114956"  # Replace with your subnet ID
+}
+
+variable "key_name" {
+  description = "The name of the SSH key pair"
+  type        = string
+  default     = "keypairterraform"  # Replace with your key pair name
 }
